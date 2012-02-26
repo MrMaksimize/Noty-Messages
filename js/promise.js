0ec21938de0cd3df7f2b669d6e03bad7c94f4882
Maksim Pecherskiy <max@maksimize.com>
2012-02-25 22:37:44 -0600

    queues working now, needs in drupal testing

--- js/promise.js
+++ js/promise.js
@@ -1,3 +1,23 @@
+/*!
+ * Noty Helpers Javascript From JQuery Javascript Library
+ *
+ * Ported by Maksim Pecherskiy.  Original Licensing:
+ *
+ * http://jquery.com/
+ *
+ * Copyright 2011, John Resig
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
+ *
+ * Includes Sizzle.js
+ * http://sizzlejs.com/
+ * Copyright 2011, The Dojo Foundation
+ * Released under the MIT, BSD, and GPL Licenses.
+ *
+ * Date: Mon Nov 21 21:11:03 2011 -0500
+ */
+
+
 (function(){
 
   // String to Object flags format cache
@@ -13,6 +33,176 @@ function createFlags( flags ) {
   }
   return object;
 }
+
+jQuery.extend({
+
+  _mark: function( elem, type ) {
+    if ( elem ) {
+      type = (type || "fx") + "mark";
+      jQuery.data( elem, type, (jQuery.data(elem,type,undefined,true) || 0) + 1, true );
+    }
+  },
+
+  _unmark: function( force, elem, type ) {
+    if ( force !== true ) {
+      type = elem;
+      elem = force;
+      force = false;
+    }
+    if ( elem ) {
+      type = type || "fx";
+      var key = type + "mark",
+        count = force ? 0 : ( (jQuery.data( elem, key, undefined, true) || 1 ) - 1 );
+      if ( count ) {
+        jQuery.data( elem, key, count, true );
+      } else {
+        jQuery.removeData( elem, key, true );
+        handleQueueMarkDefer( elem, type, "mark" );
+      }
+    }
+  },
+
+  queue: function( elem, type, data ) {
+    if ( elem ) {
+      type = (type || "fx") + "queue";
+      var q = jQuery.data( elem, type, undefined, true );
+      // Speed up dequeue by getting out quickly if this is just a lookup
+      if ( data ) {
+        if ( !q || jQuery.isArray(data) ) {
+          q = jQuery.data( elem, type, jQuery.makeArray(data), true );
+        } else {
+          q.push( data );
+        }
+      }
+      return q || [];
+    }
+  },
+
+  dequeue: function( elem, type ) {
+    type = type || "fx";
+
+    var queue = jQuery.queue( elem, type ),
+      fn = queue.shift(),
+      defer;
+
+    // If the fx queue is dequeued, always remove the progress sentinel
+    if ( fn === "inprogress" ) {
+      fn = queue.shift();
+    }
+
+    if ( fn ) {
+      // Add a progress sentinel to prevent the fx queue from being
+      // automatically dequeued
+      if ( type === "fx" ) {
+        queue.unshift("inprogress");
+      }
+
+      fn.call(elem, function() {
+        jQuery.dequeue(elem, type);
+      });
+    }
+
+    if ( !queue.length ) {
+      jQuery.removeData( elem, type + "queue", true );
+      handleQueueMarkDefer( elem, type, "queue" );
+    }
+  }
+});
+
+jQuery.fn.extend({
+  queue: function( type, data ) {
+    if ( typeof type !== "string" ) {
+      data = type;
+      type = "fx";
+    }
+
+    if ( data === undefined ) {
+      return jQuery.queue( this[0], type );
+    }
+    return this.each(function() {
+      var queue = jQuery.queue( this, type, data );
+
+      if ( type === "fx" && queue[0] !== "inprogress" ) {
+        jQuery.dequeue( this, type );
+      }
+    });
+  },
+  dequeue: function( type ) {
+    return this.each(function() {
+      jQuery.dequeue( this, type );
+    });
+  },
+  // Based off of the plugin by Clint Helfers, with permission.
+  // http://blindsignals.com/index.php/2009/07/jquery-delay/
+  delay: function( time, type ) {
+    time = jQuery.fx ? jQuery.fx.speeds[time] || time : time;
+    type = type || "fx";
+
+    return this.queue( type, function() {
+      var elem = this;
+      setTimeout(function() {
+        jQuery.dequeue( elem, type );
+      }, time );
+    });
+  },
+  clearQueue: function( type ) {
+    return this.queue( type || "fx", [] );
+  },
+  // Get a promise resolved when queues of a certain type
+  // are emptied (fx is the type by default)
+  promise: function( type, object ) {
+    if ( typeof type !== "string" ) {
+      object = type;
+      type = undefined;
+    }
+    type = type || "fx";
+    var defer = jQuery.Deferred(),
+      elements = this,
+      i = elements.length,
+      count = 1,
+      deferDataKey = type + "defer",
+      queueDataKey = type + "queue",
+      markDataKey = type + "mark",
+      tmp;
+    function resolve() {
+      if ( !( --count ) ) {
+        defer.resolveWith( elements, [ elements ] );
+      }
+    }
+    while( i-- ) {
+      if (( tmp = jQuery.data( elements[ i ], deferDataKey, undefined, true ) ||
+          ( jQuery.data( elements[ i ], queueDataKey, undefined, true ) ||
+            jQuery.data( elements[ i ], markDataKey, undefined, true ) ) &&
+          jQuery.data( elements[ i ], deferDataKey, jQuery._Deferred(), true ) )) {
+        count++;
+        tmp.done( resolve );
+      }
+    }
+    resolve();
+    return defer.promise();
+  }
+});
+
+function handleQueueMarkDefer( elem, type, src ) {
+  var deferDataKey = type + "defer",
+    queueDataKey = type + "queue",
+    markDataKey = type + "mark",
+    defer = jQuery._data( elem, deferDataKey );
+  if ( defer &&
+    ( src === "queue" || !jQuery._data(elem, queueDataKey) ) &&
+    ( src === "mark" || !jQuery._data(elem, markDataKey) ) ) {
+    // Give room for hard-coded callbacks to fire first
+    // and eventually mark/queue something else on the element
+    setTimeout( function() {
+      if ( !jQuery._data( elem, queueDataKey ) &&
+        !jQuery._data( elem, markDataKey ) ) {
+        jQuery.removeData( elem, deferDataKey, true );
+        defer.fire();
+      }
+    }, 0 );
+  }
+}
+
 
 
 jQuery.Callbacks = function( flags ) {
